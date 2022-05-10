115afe9b6680bdb440206d8c43f6f22c6338bfb3
junpatatous <mukdol1127@naver.com>
2022-05-10 23:43:52 +0900

    5/10 TIL

---
+++ app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "jun";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + myName);
+
+myName = "jun";
+
+console.log("your new name is " + myName)
